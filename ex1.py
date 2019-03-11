a885c0f448de17a1eb4f6f455e1c29b50e3072f1
seandi <eandi.simo@outlook.it>
2019-03-11 16:51:19 +0100

    Ex1 solution

--- ex1.py
+++ ex1.py
@@ -1,1 +1,18 @@
 # test
+
+while True:
+
+    n1 = input("Insert first number: ")
+    if not n1:
+        break
+    n2 = input("Insert second number: ")
+    if not n2:
+        break
+    try:
+        n1 = float(n1)
+        n2 = float(n2)
+        print(n1, "+", n2, "=", n1+n2)
+    except ValueError:
+        print("Input number is not valid")
+        continue
+
